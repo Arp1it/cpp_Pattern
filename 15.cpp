7148d5e90a6cf86692f3a20b507b34736ba544fa
Arpit <arpitjaiswal9526@gmail.com>
2025-10-22 22:11:13 +0530

    Pattern print of triangle but different

---
+++ 15.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main(){
+    int k = 5;
+
+    for (int i = 0; i < k; i++)
+    {
+        for (int j = ((k-i)-1); j > 0; j--)
+        {
+            cout<<"  ";
+        }
+
+        for (int j = 1; j <= i+1; j++)
+        {
+            cout<<j<<" ";
+        }
+
+        for (int j = 1; j <= i; j++)
+        {
+            cout<<j<<" ";
+        }
+        
+        
+        cout<<endl;
+    }
+    
+
+    return 0;
+}
